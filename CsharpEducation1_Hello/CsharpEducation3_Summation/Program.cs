0ca3015379e6a7dce63167ee64e0cc9bbaa643fe
BogdanKorolev <bkorolevjob@gmail.com>
2022-07-09 12:08:36 +0300

    CsharEducation3_Summation updated

--- CsharpEducation1_Hello/CsharpEducation3_Summation/Program.cs
+++ CsharpEducation1_Hello/CsharpEducation3_Summation/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Summation!");
+﻿int numberA = 31;
+int numberB =  5;
+int result = numberA + numberB;
+Console.WriteLine(result);
